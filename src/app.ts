18e3f19ecc4ef6e30993ec06a037388acc623438
Gabriel Portela Gomes <gabrielgomesportela@gmail.com>
2023-05-24 22:19:18 -0300

    initial configs

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import 'reflect-metadata';
+import 'express-async-errors';
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+app
+  .use(cors())
+  .use(express.json())
+  .get('/health', (_req, res) => res.send('OK!'));
+
+export default app;
