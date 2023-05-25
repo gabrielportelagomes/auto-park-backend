3f17bf41b2e218d6f05d8a64faf398dbdb21e4f0
Gabriel Portela Gomes <gabrielgomesportela@gmail.com>
2023-05-25 14:29:02 -0300

    feat: create user schema

--- src/protocols.ts
+++ src/protocols.ts
@@ -2,3 +2,8 @@ export type ApplicationError = {
   name: string;
   message: string;
 };
+
+export type CreateUserParams = {
+  email: string;
+  password: string;
+};
---
+++ src/schemas/users-schema.ts
@@ -0,0 +1,15 @@
+import Joi from 'joi';
+import { CreateUserParams } from '../protocols';
+
+export const createUserSchema = Joi.object<CreateUserParams>({
+  email: Joi.string().email().required(),
+  password: Joi.string()
+    .min(8)
+    .pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]+$/)
+    .required()
+    .messages({
+      'string.min': 'The password must have a minimum of {#limit} characters',
+      'string.pattern.base':
+        'The password must contain at least one lowercase letter, one uppercase letter, and one digit',
+    }),
+});
