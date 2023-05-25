da39857d27e034883e7c9f162b2a2b0f20946c80
Gabriel Portela Gomes <gabrielgomesportela@gmail.com>
2023-05-24 22:56:35 -0300

    feat: database connection

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
 import 'reflect-metadata';
 import 'express-async-errors';
-import express from 'express';
+import express, { Express } from 'express';
 import cors from 'cors';
+
+import { loadEnv, connectDb, disconnectDB } from './config';
+
+loadEnv();
 
 const app = express();
 
@@ -9,5 +13,14 @@ app
   .use(cors())
   .use(express.json())
   .get('/health', (_req, res) => res.send('OK!'));
+
+export function init(): Promise<Express> {
+  connectDb();
+  return Promise.resolve(app);
+}
+
+export async function close(): Promise<void> {
+  await disconnectDB();
+}
 
 export default app;
