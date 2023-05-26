0769f372a9058fcc4b7d52132fde478aa763248d
Gabriel Portela Gomes <gabrielgomesportela@gmail.com>
2023-05-26 17:15:32 -0300

    feat: get cash register balance request

--- src/routers/cashRegister-router.ts
+++ src/routers/cashRegister-router.ts
@@ -2,11 +2,12 @@ import { Router } from 'express';
 
 import { authenticateToken, validateBody } from '../middlewares';
 import { createCahsRegister } from '../schemas';
-import { postCashRegister } from '../controllers';
+import { getCashRegisterBalance, postCashRegister } from '../controllers';
 
 const cashRegisterRouter = Router();
 
 cashRegisterRouter.all('/', authenticateToken);
 cashRegisterRouter.post('/', validateBody(createCahsRegister), postCashRegister);
+cashRegisterRouter.get('/', getCashRegisterBalance);
 
 export { cashRegisterRouter };
