255fc47a9e1a9dae080796c028356b2abd69211b
Gabriel Portela Gomes <gabrielgomesportela@gmail.com>
2023-05-26 16:00:17 -0300

    feat: forbidden error

--- src/errors/index.ts
+++ src/errors/index.ts
@@ -3,3 +3,4 @@ export * from './conflict-error';
 export * from './invalid-credentials-error';
 export * from './unauthorized-error';
 export * from './not-found-error';
+export * from './forbidden-error';
