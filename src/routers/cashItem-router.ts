c0c42643ef68def74d34476d064889d194da9a79
Gabriel Portela Gomes <gabrielgomesportela@gmail.com>
2023-05-26 13:05:42 -0300

    feat: get all cash itens request

--- src/routers/cashItem-router.ts
+++ src/routers/cashItem-router.ts
@@ -2,11 +2,12 @@ import { Router } from 'express';
 
 import { authenticateToken, validateBody } from '../middlewares';
 import { createCahsItem } from '../schemas';
-import { postCashItem } from '../controllers';
+import { getAllCashItens, postCashItem } from '../controllers';
 
 const cashItemRouter = Router();
 
 cashItemRouter.all('/', authenticateToken);
 cashItemRouter.post('/', validateBody(createCahsItem), postCashItem);
+cashItemRouter.get('/all', getAllCashItens);
 
 export { cashItemRouter };
