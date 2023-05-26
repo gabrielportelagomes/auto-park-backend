d836b5449bfb9e127ef7d6ebdab571b6768222a0
Gabriel Portela Gomes <gabrielgomesportela@gmail.com>
2023-05-26 10:56:38 -0300

    refactor: function params

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,10 @@
 import 'reflect-metadata';
 import 'express-async-errors';
-import express, { Express } from 'express';
+import express, { Express, Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 
 import { loadEnv, connectDb, disconnectDB } from './config';
+import { ApplicationError } from './protocols';
 import { handleApplicationErrors } from './middlewares';
 import { authenticationRouter, usersRouter } from './routers';
 
@@ -14,10 +15,12 @@ const app = express();
 app
   .use(cors())
   .use(express.json())
-  .get('/health', (_req, res) => res.send('OK!'))
+  .get('/health', (_req: Request, res: Response) => res.send('OK!'))
   .use('/users', usersRouter)
   .use('/auth', authenticationRouter)
-  .use(handleApplicationErrors);
+  .use((err: Error | ApplicationError, req: Request, res: Response, next: NextFunction) => {
+    handleApplicationErrors(err, res);
+  });
 
 export function init(): Promise<Express> {
   connectDb();
