13f535e6bf7d13bd37ce9e9d1e1d11039b71e20c
Gabriel Portela Gomes <gabrielgomesportela@gmail.com>
2023-05-26 13:09:11 -0300

    feat: find all cash itens repository

--- src/repositories/cashItem-repository/index.ts
+++ src/repositories/cashItem-repository/index.ts
@@ -2,20 +2,25 @@ import { CreateCashItem } from '../../protocols';
 import { prisma } from '../../config';
 
 async function findByValue(value: number) {
-  return prisma.cashItem.findUnique({
+  return await prisma.cashItem.findUnique({
     where: { value },
   });
 }
 
 async function create(data: CreateCashItem) {
-  return prisma.cashItem.create({
+  return await prisma.cashItem.create({
     data,
   });
+}
+
+async function findAll() {
+  return await prisma.cashItem.findMany();
 }
 
 const cashItemRepository = {
   findByValue,
   create,
+  findAll,
 };
 
 export default cashItemRepository;
