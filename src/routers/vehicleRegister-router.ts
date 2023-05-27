1c42fa450a880c4c1d4fb3338d6e7a4f09f104c7
Gabriel Portela Gomes <gabrielgomesportela@gmail.com>
2023-05-27 18:50:35 -0300

    feat: get vehicle register by plate number request

--- src/routers/vehicleRegister-router.ts
+++ src/routers/vehicleRegister-router.ts
@@ -1,13 +1,18 @@
 import { Router } from 'express';
 
-import { authenticateToken, validateBody } from '../middlewares';
-import { createVehicleRegister } from '../schemas';
-import { getAllVehicleRegisters, postVehicleRegister } from '../controllers';
+import { authenticateToken, validateBody, validateParams } from '../middlewares';
+import { createVehicleRegister, findVehicleRegisterByPlateNumber } from '../schemas';
+import { getAllVehicleRegisters, getVehicleRegisterByPlateNumber, postVehicleRegister } from '../controllers';
 
 const vehicleRegisterRouter = Router();
 
 vehicleRegisterRouter.all('/*', authenticateToken);
 vehicleRegisterRouter.post('/', validateBody(createVehicleRegister), postVehicleRegister);
 vehicleRegisterRouter.get('/all', getAllVehicleRegisters);
+vehicleRegisterRouter.get(
+  '/:plate_number',
+  validateParams(findVehicleRegisterByPlateNumber),
+  getVehicleRegisterByPlateNumber,
+);
 
 export { vehicleRegisterRouter };
