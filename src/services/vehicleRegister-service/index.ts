b7a3c4e363acf1f45af1974f8b1ae237e05a2a3a
Gabriel Portela Gomes <gabrielgomesportela@gmail.com>
2023-05-27 16:08:11 -0300

    feat: create vehicle register, validate vehicle type and active register services

---
+++ src/services/vehicleRegister-service/index.ts
@@ -0,0 +1,47 @@
+import vehicleRegisterRepository from '../../repositories/vehicleRegister-repository';
+import { conflictError, notFoundError } from '../../errors';
+import vehicleTypeRepository from '../../repositories/vehicleType-repository';
+import { VehicleRegister } from '@prisma/client';
+
+export async function createVehicleRegister(
+  user_id: number,
+  vehicle_type_id: number,
+  plate_number: string,
+): Promise<VehicleRegister> {
+  await validateUniqueVehicleActive(plate_number);
+  await validateVehicleType(vehicle_type_id);
+
+  const entry_time: Date = new Date();
+  entry_time.setHours(entry_time.getHours() - 3);
+
+  const vehicleRegister = await vehicleRegisterRepository.create({
+    user_id,
+    vehicle_type_id,
+    plate_number,
+    entry_time,
+  });
+
+  return vehicleRegister;
+}
+
+async function validateUniqueVehicleActive(plate_number: string) {
+  const isActiveVehicleAlreadyRegistered = await vehicleRegisterRepository.findByPlateNumber(plate_number);
+
+  if (isActiveVehicleAlreadyRegistered) {
+    throw conflictError('This vehicle already has an active registration!');
+  }
+}
+
+async function validateVehicleType(vehicle_type_id: number) {
+  const vehicleType = await vehicleTypeRepository.findById(vehicle_type_id);
+
+  if (!vehicleType) {
+    throw notFoundError();
+  }
+}
+
+const vehicleRegisterService = {
+  createVehicleRegister,
+};
+
+export default vehicleRegisterService;
