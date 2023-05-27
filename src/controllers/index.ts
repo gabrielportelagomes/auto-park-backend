a653f7529752abc3d8c4ab428e68f92565c0c6b8
Gabriel Portela Gomes <gabrielgomesportela@gmail.com>
2023-05-27 11:43:59 -0300

    feat: post vehicle type controller

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -2,3 +2,4 @@ export * from './users-controller';
 export * from './authentication-controller';
 export * from './cashItem-controller';
 export * from './cashRegister-controller';
+export * from './vehicleType-controller';
